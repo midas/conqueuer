51cd926196c0e0b8ef33d7ffd3b752031cbde971
Jason Harrelson <jason@lookforwardenterprises.com>
2015-10-28 15:46:04 -0500

    Add work function to Conqueuer module.

--- lib/conqueuer.ex
+++ lib/conqueuer.ex
@@ -1,2 +1,12 @@
 defmodule Conqueuer do
+
+  alias Conqueuer.Util
+
+  def work( name, args \\ nil ) do
+    {foreman_name, queue_name} = Util.infer_conqueuer_collaborator_names( name )
+
+    Conqueuer.Queue.enqueue( queue_name, args )
+    Conqueuer.Foreman.work_arrived( foreman_name )
+  end
+
 end
