daf56b0f6764c78a08e8714db1af1cf3af47836d
Jason Harrelson <jason@lookforwardenterprises.com>
2015-11-17 21:49:35 -0600

    Bump version.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Conqueuer.Mixfile do
 
   def project do
     [app: :conqueuer,
-     version: "0.3.0",
+     version: "0.4.0",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      preferred_cli_env: [espec: :test],
